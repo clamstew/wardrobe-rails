55834975715771888b682488b7c9ce5ef6b9dda5
Clay Stewart <claytonmstewart@gmail.com>
2013-09-30 11:27:08 -0500

    Add failing view tests for when you load the controller as json for WardrobeItems#index and WardrobeItems#show. Add format blocks to index and show actions in controller to make tests pass

--- app/controllers/wardrobe_items_controller.rb
+++ app/controllers/wardrobe_items_controller.rb
@@ -1,5 +1,17 @@
 class WardrobeItemsController < ApplicationController
   def index
     @wardrobe_items = WardrobeItem.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @wardrobe_items }
+    end
+  end
+
+  def show
+    @wardrobe_item = WardrobeItem.find(params[:id])
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @wardrobe_item }
+    end
   end
 end
--- spec/controllers/wardrobe_items_controller_spec.rb
+++ spec/controllers/wardrobe_items_controller_spec.rb
@@ -8,5 +8,30 @@ describe WardrobeItemsController do
 
       expect(assigns('wardrobe_items')).to eq([wardrobe_item])
     end
+
+    # NB - this is a view test and should be elsewhere in rspec
+    it "returns all wardrobe items as json" do
+      wardrobe_item = WardrobeItem.create
+      get :index, :format => :json
+
+      response.body.should == [wardrobe_item].to_json
+    end
+  end
+
+  describe "GET show" do
+    it "assigns one wardrobe item as @wardrobe_item" do
+      wardrobe_item = WardrobeItem.create
+      get :show, { :id => wardrobe_item }
+
+      expect(assigns('wardrobe_item')).to eq(wardrobe_item)
+    end
+
+    # NB - this is a view test and should be elsewhere in rspec
+    it "returns one wardrobe items as json" do
+      wardrobe_item = WardrobeItem.create
+      get :show, { :id => wardrobe_item, :format => :json }
+
+      response.body.should == wardrobe_item.to_json
+    end
   end
 end
