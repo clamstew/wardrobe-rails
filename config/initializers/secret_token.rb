3853eb7392ea30bee89a3bf43aed141711ab82dc
Clay Stewart <claytonmstewart@gmail.com>
2013-09-30 10:01:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LionWitch::Application.config.secret_key_base = '7d21f29bcadc10f0a4660fe946193b87607ed5357d83f8b6da5c257a4cfc8b522481985b63473e2c35698a9088f340fc1dd7a59ba45fb3304fc5b815679306f9'
---
+++ vendor/bundle/gems/atomic-1.1.14/ext/siteconf20130930-7027-wvmb3i.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130930-7027-3e7ovy"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
